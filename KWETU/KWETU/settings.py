aedbec6b806cc8875c83b25745b67aae4527750f
mugumya21 <94521776+mugumya21@users.noreply.github.com>
2021-12-27 22:17:39 -0800

    settings update

--- KWETU/KWETU/settings.py
+++ KWETU/KWETU/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hd(qe=8-dx@q24!(32q8xk2e21o!z8a2g7*6qzvz131$bg1yt!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['businessms.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,8 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #heroku will read static files
+
 STATIC_URL = 'static/'
+
 MEDIA_URL = '/images/'
+
 STATICFILES_DIRS= [BASE_DIR /'static',
                    os.path.join(BASE_DIR, 'static')
 
